2b36c48bbd487bc0215c0e2dd20d0320079a86cc
Justin Fargione <justin@lexicalbits.com>
2015-09-18 15:13:07 -0400

    Added an environment example

---
+++ fixtures/env.example.js
@@ -0,0 +1,10 @@
+//Copy this to env.js
+module.exports = {
+    "environment": {
+        twitter_consumer_key:           ''
+        , twitter_consumer_secret:      ''
+        , twitter_access_token:         ''
+        , twitter_access_token_secret:  ''
+    }
+};
+
